acec3e451f1a4b3fa3237993a9fb2f15796749ae
Davi Silva <davisilvaphoto@gmail.com>
2026-06-18 20:58:28 -0300

    intern arrays

--- 8 - Arrays and Slices/main.go
+++ 8 - Arrays and Slices/main.go
@@ -28,4 +28,24 @@ func main() {
 	fmt.Println(slice2)
 	arr2[1] = "Pos Alt"
 	fmt.Println(slice2)
+
+	fmt.Println("---------------------")
+
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+	fmt.Println(slice3)
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+	fmt.Println(slice4)
 }

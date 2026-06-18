c72c31bc9222d1a47844fdc532ece45cd3c2752f
Davi Silva <davisilvaphoto@gmail.com>
2026-06-18 20:37:40 -0300

    arrays and slices

---
+++ 8 - Arrays and Slices/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var arr1 [5]string
+	arr1[0] = "Pos 1"
+	fmt.Println(arr1)
+
+	arr2 := [5]string{"Pos 1", "Pos 2", "Pos 3", "Pos 4", "Pos 5"}
+	fmt.Println(arr2)
+
+	arr3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(arr3)
+
+	slice := []int{2, 3, 4, 5}
+
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(arr3))
+
+	slice = append(slice, 20)
+	fmt.Println(slice)
+
+	slice2 := arr2[1:3]
+	fmt.Println(slice2)
+	arr2[1] = "Pos Alt"
+	fmt.Println(slice2)
+}
